d80d527a3718a1bf0801c2f896df9783290ee9bc
rajibkhan976 <rajibkhan976@gmail.com>
2024-04-02 23:48:08 +0800

    seaborn color palette to rgb converted is modified according to the requirements

--- rgb-seaborn.py
+++ rgb-seaborn.py
@@ -1,15 +1,31 @@
 import seaborn as sns
-
-color_pal = sns.color_palette()
-color_pal_hex = color_pal.as_hex()
-color_pal_rgb = []
-
+from matplotlib import colormaps
 
 def hex_to_rgb(hex):
   return tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
 
 
-for color in color_pal_hex:
-    color_pal_rgb.append(hex_to_rgb(color.lstrip('#')))
+def convert_seaborn_color_to_rgb(paletteName):
+  color_pal = None
+  color_pal_hex = None
+  color_pal_rgb = []
+  if paletteName in list(colormaps):
+    try:
+      color_pal = sns.color_palette(paletteName)
+    except:
+      print("Palette Name is not correct")
+  if color_pal:
+    try:
+      color_pal_hex = color_pal.as_hex()
+    except:
+      print("Color Palette is empty")
+  if color_pal_hex:
+    try:
+      for color in color_pal_hex:
+        color_pal_rgb.append(hex_to_rgb(color.lstrip('#')))
+    except:
+      print("Color Palette is empty")
+  return color_pal_rgb
 
-print(color_pal_rgb)
+
+print(convert_seaborn_color_to_rgb("autumn"))
